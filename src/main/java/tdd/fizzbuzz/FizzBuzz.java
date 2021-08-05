06f49c767bd0ae84de82935666964f77c83ab284
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-05 13:49:25 +0800

    [FizzBuzz][Juan Carlos Salvador] feat: implement feature to return fizz if number is multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,10 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public String countOff(int orderNumber) {
 
+        if (orderNumber % 3 == 0) {
+            return "Fizz";
+        }
+
         return String.valueOf(orderNumber);
     }
 }
