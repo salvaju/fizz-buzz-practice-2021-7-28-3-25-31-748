eea8fbeec8e8b437be0540a0033b0425989b8352
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-05 14:05:10 +0800

    [FizzBuzz][Juan Carlos Salvador] refactor: created a constant for Whizz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,7 @@ public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
 
     public String countOff(int orderNumber) {
 
@@ -12,7 +13,7 @@ public class FizzBuzz {
         } else if (orderNumber % 5 == 0) {
             return BUZZ;
         } else if (orderNumber % 7 == 0) {
-            return "Whizz";
+            return WHIZZ;
         }
 
         return String.valueOf(orderNumber);
