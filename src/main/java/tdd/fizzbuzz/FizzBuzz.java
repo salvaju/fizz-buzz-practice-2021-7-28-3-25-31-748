f1ec60db9abc545dae4ec18563490d6462aa7a46
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-05 14:00:07 +0800

    [FizzBuzz][Juan Carlos Salvador] refactor: create constant for Buzz and change test name

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,13 +3,14 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
 
     public String countOff(int orderNumber) {
 
         if (orderNumber % 3 == 0) {
             return FIZZ;
         } else if (orderNumber % 5 == 0) {
-            return "Buzz";
+            return BUZZ;
         }
 
         return String.valueOf(orderNumber);
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -33,7 +33,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void should_return_Fizz_when_count_off_given_is_a_number_multiple_of_5() {
+    void should_return_Buzz_when_count_off_given_is_a_number_multiple_of_5() {
         //given
         int orderNumber = 5;
         FizzBuzz fizzBuzz = new FizzBuzz();
